decf9aa879bef2e957835f0188a8cfdf72b724c8
Ankit_Anmol <87054411+Quantum-Codes@users.noreply.github.com>
2021-07-27 10:07:15 +0000

    Error

--- main.py
+++ main.py
@@ -14,33 +14,46 @@ result = soup.find_all("li", class_="top-level-reply")
 #print(result[6])
 #exit()
 
-replies = result[7].find("ul", class_="replies")
-if replies.text == "":
+def get_replies(count):
+  replies = result[count].find("ul", class_="replies")
+  if replies.text == "":
     print("No content!")
-user = replies.find_all("div", class_="info")
-#print(user)
-all_replies = []
-for i in range(0, len(user)):
-	username = user[i].find("div", class_="name")
-	username = username.find("a").text
-	content = user[i].find("div", class_="content").text
-	username = username.strip().replace("\n","")
-	content = content.strip().replace("\n","")
-	search = re.search("data-comment-id=", str(result[i]))
-	index = search.span()[1]
-	data = str(result[i])[index + 1:]
-	i = 0
-	id = ""
-	while data[i] != '"':
-		id += data[i]
-		i += 1
-	id = int(id)
-	reply = {"id":id, "username" : username, "comment" : 	content.replace("                   ","")}
-	all_replies.append(reply)
-	print(reply)
-print()
-print(all_replies)
-
+  user = replies.find_all("div", class_="info")
+  #print(user)
+  all_replies = []
+  for i in range(0, len(user)):
+	  username = user[i].find("div", class_="name")
+	  username = username.find("a").text
+	  content = user[i].find("div", class_="content").text
+	  username = username.strip().replace("\n","")
+	  content = content.strip().replace("\n","")
+	  search = re.search("data-comment-id=", str(result[i]))
+	  index = search.span()[1]
+	  data = str(result[i])[index + 1:]
+	  i = 0
+	  id = ""
+	  while data[i] != '"':
+		  id += data[i]
+		  i += 1
+	  id = int(id)
+	  
+	  """
+    search = re.search("title=", str(result[i]))
+    #print(search)
+    index = search.span()[1]
+    #print(index)
+    data = str(result[i])[index + 1:]
+    #print(data)
+    i = 0
+    comment_time = ""
+    while data[i] != '"':
+       comment_time += data[i]
+       i += 1 
+    """
+    reply = {"id":id, "username" : username, "comment" : 	content.replace("                   ","")}
+    all_replies.append(reply)
+	return all_replies
+#print(get_replies(0)) 
 
 '''
 search = re.search("title=", str(result[0]))
@@ -57,58 +70,57 @@ while data[i] != '"':
 
 print(id)
 '''
-exit()
-'''
 for i in range(0, len(result)):
-	user = result[i].find("div", class_="comment")
-	user = user.find("div", class_="info")
-	user = user.find("div", class_="name")
-	user = user.find("a")
-	user = user.text
-
-	content = result[i].find("div", class_="comment")
-	content = content.find("div", class_="info")
-	content = content.find("div", class_="content")
-	content = content.text.strip()
-
-
-	search = re.search("data-comment-id=", str(result[i]))
-	#print(search)
-	index = search.span()[1]
-	#print(index)
-	data = str(result[i])[index + 1:]
-	#print(data)
-	i = 0
-	id = ""
-	while data[i] != '"':
-		id += data[i]
-		i += 1
-	id = int(id)
-	#print(id)
-	
-	search = re.search("title=", str(result[i]))
-	#print(search)
-	index = search.span()[1]
-	#print(index)
-	data = str(result[i])[index + 1:]
-	#print(data)
-	i = 0
-	comment_time = ""
-	while data[i] != '"':
-		comment_time += data[i]
-		i += 1
-
-
-	comment = {
+  user = result[i].find("div", class_="comment")
+  replies = get_replies(i)
+  user = user.find("div", class_="info")
+  user = user.find("div", class_="name")
+  user = user.find("a")
+  user = user.text
+  
+  content = result[i].find("div", class_="comment")
+  content = content.find("div", class_="info")
+  content = content.find("div", class_="content")
+  content = content.text.strip()
+  
+  search = re.search("data-comment-id=", str(result[i]))
+  #print(search)
+  index = search.span()[1]
+  #print(index)
+  data = str(result[i])[index + 1:]
+  #print(data)
+  i = 0
+  id = ""
+  while data[i] != '"':
+    id += data[i]
+    i += 1
+  id = int(id)
+  #print(id)
+  
+  search = re.search("title=", str(result[i]))
+  #print(search)
+  index = search.span()[1]
+  #print(index)
+  data = str(result[i])[index + 1:]
+  #print(data)
+  i = 0
+  comment_time = ""
+  while data[i] != '"':
+    comment_time += data[i]
+    i += 1
+  if len(replies) == 0:
+    parent = False
+  else:
+    parent = True
+  comment = {
 	    "Username": user,
 	    "Content": content,
 	    "Time": comment_time,
 	    "ID": id,
-	    "IsParent": True,
+	    "IsParent": parent,
+	    "Replies": replies,
 	    "CommentID": id
-	}
-
-	comment = json.dumps(comment)
-	comment = json.loads(comment)
-	print(comment)
-'''
+  }
+  comment = json.dumps(comment)
+  comment = json.loads(comment)
+  print(comment)
